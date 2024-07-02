715e348202fb4f199ae99305d3abae9ea0aed449
Artem Vozhov <vozhov.artem1@gmail.com>
2024-07-02 13:07:10 +0300

    dtos

--- src/main/java/com/vozh/art/bookservice/dto/BookRequst.java
+++ src/main/java/com/vozh/art/bookservice/dto/BookRequst.java
@@ -1,4 +1,20 @@
 package com.vozh.art.bookservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookRequst {
+    String title;
+    String author;
+    String isbn;
+    LocalDate publishedDate;
 }
